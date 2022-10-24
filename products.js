c3ab911b91b1155e28c553c611130be6ab9ac965
kmerck88 <Kevin.Merck88@gmail.com>
2022-10-24 04:33:28 -0400

    Added products to page

---
+++ products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 0,
+    name: "Ice coffee",
+    price: 3.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "img/iceCoffee.jpg",
+  },
+  {
+    id: 1,
+    name: "Cappuccino",
+    price: 3.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "img/Cappuccino.jpg",
+  },
+  {
+    id: 2,
+    name: "Latte",
+    price: 2.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "img/Latte.jpg",
+  },
+
+ 
+
+];
